1e95c7aa0a6bf75595b391a846ed927c73d4c830
Guilherme Cherubini <gabreu@thoughtworks.com>
2018-05-07 21:44:10 -0300

    improving file persistence

--- app/src/main/java/com/example/guilherme/mynewapplication/HomeActivity.java
+++ app/src/main/java/com/example/guilherme/mynewapplication/HomeActivity.java
@@ -27,13 +27,13 @@ import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String AUTHORITIES = "com.example.guilherme.mynewapplication.fileprovider";
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_IMAGE_CAPTURE_AND_STORE = 2;
     private static final int CAMERA_PERMISSION_CODE = 1;
     private static final int WRITE_STORAGE_PERMISSION_CODE = 2;
-    private String storedImageFilePath;
+    private Uri storedImageFilePath;
     private ImageView imageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +41,6 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         imageView = findViewById(R.id.image_view);
-
-        Picasso.get().load("http://www.rmmfi.org/wp-content/uploads/2017/05/image-coming-soon.png")
-                .fit()
-                .into(imageView);
     }
 
 
@@ -115,6 +111,13 @@ public class HomeActivity extends AppCompatActivity {
 
     private void takePictureAndStoreInDevice() {
         if (requestPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)) return;
+        if (requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_STORAGE_PERMISSION_CODE)) return;
+
+
+        if(!isExternalStorageWritable()) {
+            Toast.makeText(this, "External storage unavailable", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -131,33 +134,39 @@ public class HomeActivity extends AppCompatActivity {
             // Continue only if the File was successfully created
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.guilherme.mynewapplication.fileprovider",
+                        AUTHORITIES,
                         photoFile);
+                storedImageFilePath = photoURI;
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_AND_STORE);
             }
         }
     }
 
+
+    /**
+     * Because the external storage might be unavailable—such as when the user has mounted the storage to a PC
+     * or has removed the SD card that provides the external storage—you should always verify that the volume
+     * is available before accessing it. You can query the external storage state by calling getExternalStorageState().
+     * If the returned state is MEDIA_MOUNTED, then you can read and write your files. If it's MEDIA_MOUNTED_READ_ONLY, you can only read the files.
+     * */
+
+    public boolean isExternalStorageWritable() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir      /* directory */
-//        );
 
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
         File image = new File(storageDir, imageFileName + ".jpeg");
         image.createNewFile();
 
-
         // Save a file: path for use with ACTION_VIEW intents
-        storedImageFilePath = image.getAbsolutePath();
+        // imageAbsolutePath = image.getAbsolutePath();
         return image;
     }
 }
